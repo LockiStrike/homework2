e5b4daca9b0a0466e2d30818931d113a49e2f47c
Vlad Bilanchuk <burning1848@gmail.com>
2017-06-10 18:49:31 +0300

    done

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,26 @@
 
 # exercise object model
 module ObjectModel
+  # module with linux fork
+  module LinuxFriendly
+    def fork_process
+      super
+    end
+  end
+
+  # class with own fork and inspect
+  class Desktop
+    def fork_process
+      'Parent: allocate memory'
+    end
+
+    def mine_bitcoins
+      inspect
+    end
+  end
+
+  # child with prepended module
+  class Laptop < Desktop
+    prepend LinuxFriendly
+  end
 end
--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -2,4 +2,41 @@
 
 # exercise state and behaviour
 module StateAndBehaviour
+  # class
+  class Car
+    attr_accessor :color, :model, :current_speed, :sets, :year
+
+    def self.default_car
+      Car.new {}
+    end
+
+    def initialize(args)
+      raise ArgumentError if args.class != Hash
+      if args.empty?
+        set_deafult_args
+      else
+        args.each do |key, value|
+          instance_variable_set("@#{key}", value) unless value.nil?
+        end
+      end
+      self.current_speed = 15
+    end
+
+    def speed_up(value)
+      self.current_speed = value unless value.nil?
+    end
+
+    def push_break(value)
+      self.current_speed = current_speed - value if value <= current_speed
+    end
+
+    private
+
+    def set_deafult_args
+      self.sets = 4
+      self.color = 'green'
+      self.model = 'Zaporojec'
+      self.year = 3222
+    end
+  end
 end
